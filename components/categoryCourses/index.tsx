8eb624e02c38fd7cc4b58a4d57ebd2457a92f158
muzamilkm <mkaleem.bscs22seecs@seecs.edu.pk>
2023-12-17 16:20:42 +0500

    added course name to explore by category

--- components/categoryCourses/index.tsx
+++ components/categoryCourses/index.tsx
@@ -27,6 +27,22 @@ const CategoryCourses = () => {
   }, [category]);
 
   return (
+    <div className="flex flex-col justify-center items-center gap-16">
+  <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 px-[78px] py-16 bg-white">
+    <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 gap-8">
+      <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 gap-8">
+        <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 gap-6">
+          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative">
+            <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-center text-black">
+              {category}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+      
     <div className="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 overflow-x-auto gap-16">
   <div className="flex flex-wrap justify-start items-start flex-grow-0 flex-shrink-0 max-w-[1312px] gap-8">
     <CourseCard img="/courseImg.png" category={category} title="Course Name" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros."/>
@@ -36,7 +52,8 @@ const CategoryCourses = () => {
     <CourseCard img="/courseImg.png" category={category} title="Course Name" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros."/>
     <CourseCard img="/courseImg.png" category={category} title="Course Name" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros."/>
   </div>
-</div>
+    </div>
+    </div>
 
   );
 };
