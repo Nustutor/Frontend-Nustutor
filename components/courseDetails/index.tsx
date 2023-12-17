ff2b80cb6d705a9547dd57c27b4afd8d56f1773b
muzamilkm <mkaleem.bscs22seecs@seecs.edu.pk>
2023-12-17 19:43:49 +0500

    added routing for enrolling

--- components/courseDetails/index.tsx
+++ components/courseDetails/index.tsx
@@ -1,12 +1,17 @@
+"use client"
 import React from 'react'
 import CourseListing from './courseListing';
+import { usePathname } from 'next/navigation';
 
 const CoursePage = () => {
+
+  const title = decodeURIComponent(usePathname().split('/home/')[1]);
+
   return (
     <CourseListing
       Level="UG"
       Category="Computer Science"
-      Title={`Programming Fundamentals`}
+      Title={title}
       CourseDescription="4 Credit Hour Course" 
       CourseHero={"/courseListing.png"}  />
   )
