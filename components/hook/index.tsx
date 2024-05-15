881421ddfd1fcf5ed9f1992cf964b2a13a6aa553
muzamilkm <mkaleem.bscs22seecs@seecs.edu.pk>
2024-05-16 00:04:13 +0500

    Landing responsive done

--- components/hook/index.tsx
+++ components/hook/index.tsx
@@ -27,23 +27,22 @@ const SignupHook = () => {
       }
     }, 700);
   }
-  return (
-    <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0  overflow-hidden gap-2.5 py-24 bg-gray-50">
-  <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[1280px]">
-    <div>{loading && <Loader LoaderText={'Loading'} />}
-    <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 w-[640px] relative gap-6"
-    onClick = {signupClick}>
-      <p className="self-stretch flex-grow-0 flex-shrink-0 w-[640px] text-4xl font-bold text-center text-[#111928]">
+  return(
+  <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 py-24 bg-gray-50">
+  <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 max-w-[1280px] w-full">
+    {loading && <Loader LoaderText={'Loading'} />}
+    <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 max-w-[640px] w-full relative gap-6" onClick={signupClick}>
+      <p className="self-stretch flex-grow-0 flex-shrink-0 max-w-[640px] w-full text-4xl font-bold text-center text-[#111928]">
         Start for free trial today
       </p>
-      <p className="self-stretch flex-grow-0 flex-shrink-0 w-[640px] text-xl text-center text-gray-500">
-        Nustutor is completely free, as long as @momin.ahmed agrees :){" "}
+      <p className="self-stretch flex-grow-0 flex-shrink-0 max-w-[640px] w-full text-xl text-center text-gray-500">
+        Nustutor is completely free, as long as @momin.ahmed agrees :)
       </p>
-    <CTA/>
-    </div>
+      <CTA />
     </div>
   </div>
 </div>
+
   )
 }
 
--- components/team/index.tsx
+++ components/team/index.tsx
@@ -7,7 +7,40 @@ const Team = () => {
       className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0  overflow-hidden gap-2.5 pb-24 bg-white"
       id="aboutus"
     >
-      <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0  overflow-hidden gap-20 px-16 py-28 bg-[#f7f7f7]">
+      <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 pb-12 md:pb-24 bg-white md:hidden">
+    <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 overflow-hidden gap-10 md:gap-20 px-6 md:px-16 py-14 md:py-28 bg-[#f7f7f7]">
+      <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 w-full md:w-[768px] gap-4">
+        <div className="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-4">
+          <p className="self-stretch flex-grow-0 flex-shrink-0 w-full text-3xl md:text-5xl font-bold text-center text-black">
+            Our team
+          </p>
+          <p className="self-stretch flex-grow-0 flex-shrink-0 w-full text-base md:text-lg text-center text-black">
+          Tech trailblazers shaping tomorrow's solutions.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 w-full md:w-[1280px] gap-8 md:gap-24">
+        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 gap-8 md:gap-16">
+        <Member
+  img="./muz.png"
+  role="Frontend Developer & Database Engineer"
+  name="Muzamil Kaleem"
+/>
+<Member
+  img="./hadi.jpg"
+  role="Team Lead & Lead Backend Developer"
+  name="Hadi Khan"
+/>
+<Member
+  img="./aman.png"
+  role="Lead Frontend Developer & UI/UX Designer"
+  name="Syed Aman Hussain"
+/>
+        </div>
+      </div>
+    </div>
+  </div>
+      <div className="hidden md:flex flex-col justify-start items-center flex-grow-0 flex-shrink-0  overflow-hidden gap-20 px-16 py-28 bg-[#f7f7f7]">
         <div className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 w-[768px] gap-4">
           <div className="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-6">
             <p className="self-stretch flex-grow-0 flex-shrink-0 w-[768px] text-5xl font-bold text-center text-black">
